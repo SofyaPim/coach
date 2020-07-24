08a8ee678d4ea94b40bc47e80d09b23c499ce2c0
Sofya <48826470+SofyaPim@users.noreply.github.com>
2020-07-24 16:13:46 +0300

    made a change branch

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './styles/style.css'
+
+import * as $ from 'jquery/dist/jquery'
+import 'popper.js'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import adaptiveMenu from './js/adaptiveMenu.js'
+
+ 
+
+
